13cb2c8bd090d25c67be817226ebca6cc8fcacfa
Raith <raithsphere@gmail.com>
2026-04-04 23:31:42 +0100

    1.21.11

--- src/main/java/net/raith/pishock/PiShockConfig.java
+++ src/main/java/net/raith/pishock/PiShockConfig.java
@@ -1,7 +1,6 @@
 package net.raith.pishock;
 
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.config.ModConfigEvent;
@@ -42,11 +41,6 @@ public class PiShockConfig
             .defineInRange("PiShock_DeathIntensity", 80, 1, 100);
 
     static final ModConfigSpec SPEC = BUILDER.build();
-
-    private static boolean validateItemName(final Object obj)
-    {
-        return obj instanceof String itemName && BuiltInRegistries.ITEM.containsKey(ResourceLocation.parse(itemName));
-    }
 
     @SubscribeEvent
     static void onLoad(final ModConfigEvent event)
